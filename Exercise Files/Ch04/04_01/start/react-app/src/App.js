a45dc7a02263c5043cf3f2f59137176f903b04fc
Jotony Guerra <jotonyguerra@gmail.com>
2022-07-19 19:13:52 -0700

    more useEffects

--- Exercise Files/Ch04/04_01/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_01/start/react-app/src/App.js
@@ -5,10 +5,15 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("Tired");
 
   useEffect(() => {
     console.log(`It's ${emotion} right now`);
   }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`);
+  }, [secondary]);
 
   return (
     <div className="App">
@@ -18,6 +23,10 @@ function App() {
       </button>
       <button onClick={() => setEmotion("Angry")}>
         Angry
+      </button>
+      <h2>Current secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary("Grateful")}>
+        Grateful
       </button>
     </div>
   );
