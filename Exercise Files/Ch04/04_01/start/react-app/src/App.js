e3db87dbdb478dc2a43905a051b7f30ef6aee657
Jotony Guerra <jotonyguerra@gmail.com>
2022-07-19 19:21:27 -0700

    useReducer replaces useState

--- Exercise Files/Ch04/04_01/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_01/start/react-app/src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-
-//const [firstCity, secondCity, ThridCity] = ["Tokyo", "Las Vegas", "New York"];
+import { useState, useEffect, useReducer } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("Tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`);
-  }, [secondary]);
-
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className="App">
-      <h1>Current Emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("Sad")}>
-        Sad
-      </button>
-      <button onClick={() => setEmotion("Angry")}>
-        Angry
-      </button>
-      <h2>Current secondary emotion is {secondary}</h2>
-      <button onClick={() => setSecondary("Grateful")}>
-        Grateful
-      </button>
+      <input type="checkbox"
+        value={checked}
+        onChange={setChecked}
+      />
+      <label>{checked ? "Checked" : "Not Checked"}</label>
     </div>
   );
 }
