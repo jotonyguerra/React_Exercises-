5385a28c882761dd25022f58ccdbcb3faf0cf747
Jotony Guerra <jotonyguerra@gmail.com>
2022-07-20 18:15:46 -0700

    react Render

--- Exercise Files/Ch04/04_01/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_01/start/react-app/src/App.js
@@ -1,75 +1,39 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser({name, location, avatar}) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={150} alt="user avatar"/>
-    </div>
-  );
-}
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 }
+];
 
-const query = `
-query {
-  allLifts {
-    name
-    elevationGain
-  	status
-  }
-}
-`;
-
-const opts = {
-  method: "POST",
-  headers: {"Content-Type": "application/json"},
-  body: JSON.stringify({query})
-}
-
-function Lift({name, elevationGain, status}) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{elevationGain} {status}</p>
-    </div>
-  );
-}
-
-function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      `https://snowtooth.moonhighway.com/`,
-      opts
-    )
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if(loading) return <h1>Loading...</h1>
-  if (error)
-    return <pre>{JSON.stringify(error)}</pre>
-  if (!data) return null;
-  console.log(data, "DATA")
-  return (
-    <div>
-      {data.data.allLifts.map((lift) => (
-      <Lift 
-        name={lift.name}
-        elevationGain={lift.elevationGain}
-        status={lift.status}
-        
-      />
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>
+          {renderItem(item)}
+        </li>
       ))}
-
-    </div>
-  )
+    </ul>
+  );
 }
+function App() {
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation} ft.
+        </>
+      )}
+    />
+  );
+}
+
 
 export default App;
